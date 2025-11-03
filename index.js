687ce718161deec66532454803210ef0954ec402
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-03 08:46:49 +0700

    Menambahkan endpoint data buku baru

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ console.log('Server started on port 3000');
 // Sinkronisasi model ke database (membuat tabel jika belum ada)
 db.sequelize.sync()
 .then((result) => {
+app.listen(PORT, () => {
+console.log(`Server started on port ${PORT}`);
+})
+})
+.catch((err) => {
+console.log(err);
+})
+// Endpoint untuk menambah data buku baru
+app.post("/buku", async (req, res) => {
+const data = req.body;
+try {
+const buku = await db.buku.create(data);
+res.send(buku);
+} catch (err) {
+res.send(err);
+}
+});
